575b6ac61d9261fdcbcc437593c828a2ad6be382
spam80 <45275318+spam80@users.noreply.github.com>
2018-12-12 13:43:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on('message', message => { // لا تغير شئ عشان ما تخرب �
 if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
+      for(let x = 0; x < 1000000; x++) {
         message.channel.send(`**Fuck OFF :notes: .Lumiz , Fuck OFF :notes:** [ " ${x} " ]`)
           .then(m => {
             count++;
@@ -71,7 +71,7 @@ client2.on('message', message => { // لا تغير شئ عشان ما تخرب 
 if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
+      for(let x = 0; x < 1000000; x++) {
         message.channel.send(`** Fuck OFF :notes: .Lumiz , Fuck OFF :notes: **[ " ${x} " ]`)
           .then(m => {
             count++;
